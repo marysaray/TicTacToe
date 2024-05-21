f3a2d2341646a97df6f423e8f992f8de473dd4c1
marysaray <msaray.ms@outlook.com>
2024-05-21 16:14:22 -0700

    Buttons for the rows, columns and new game feature implemented.

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.tictactoe
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,73 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val playerX = "X"
+        val playerO = "O"
+        // Top row on the left corner
+        val row1col1 = findViewById<Button>(R.id.row1col1Button)
+        row1col1.setOnClickListener {
+            row1col1.text = playerX
+        }
+        // Top row in the middle
+        val row1col2 = findViewById<Button>(R.id.row1col2Button)
+        row1col2.setOnClickListener {
+            row1col2.text = playerO
+        }
+        // Top row on the right corner
+        val row1col3 = findViewById<Button>(R.id.row1col3Button)
+        row1col3.setOnClickListener {
+            row1col3.text = playerO
+        }
+        // Middle row on the left side
+        val row2col1 = findViewById<Button>(R.id.row2col1Button)
+        row2col1.setOnClickListener {
+            row2col1.text = playerX
+        }
+        // Middle row in the middle
+        val row2col2 = findViewById<Button>(R.id.row2col2Button)
+        row2col2.setOnClickListener {
+            row2col2.text = playerX
+        }
+        // Middle row on the right side
+        val row2col3 = findViewById<Button>(R.id.row2col3Button)
+        row2col3.setOnClickListener {
+            row2col3.text = playerO
+        }
+        // Bottom row on the left corner
+        val row3col1 = findViewById<Button>(R.id.row3col1Button)
+        row3col1.setOnClickListener {
+            row3col1.text = playerO
+        }
+        // Bottom row in the middle
+        val row3col2 = findViewById<Button>(R.id.row3col2Button)
+        row3col2.setOnClickListener {
+            row3col2.text = playerO
+        }
+        // Bottom row on the right corner
+        val row3col3 = findViewById<Button>(R.id.row3col3Button)
+        row3col3.setOnClickListener {
+            row3col3.text = playerX
+        }
+        val displayPlayer = findViewById<TextView>(R.id.displayTextId)
+        val newGame = findViewById<Button>(R.id.newGameButton)
+        newGame.setOnClickListener {
+            "Player X's turn".also { displayPlayer.text = it }
+            val nineBoxesText = arrayOf(
+                  row1col1, row1col2, row1col3
+                , row2col1, row2col2, row2col3
+                , row3col1, row3col2, row3col3)
+            for (box in nineBoxesText) {
+                row1col1.text = " "
+                row1col2.text = " "
+                row1col3.text = " "
+                row2col1.text = " "
+                row2col2.text = " "
+                row2col3.text = " "
+                row3col1.text = " "
+                row3col2.text = " "
+                row3col3.text = " "
+            }
         }
     }
 }
